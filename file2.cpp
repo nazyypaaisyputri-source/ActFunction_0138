433b203c9c461368340714407dc402270d164e46
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-03 21:21:42 +0700

    implementasi fungsi input dan hitung luas

---
+++ file2.cpp
@@ -0,0 +1,14 @@
+// library
+#include <iostream>
+using namespace std;
+
+// deklarasi global
+int p, l, luas;
+
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout  << "Maukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
